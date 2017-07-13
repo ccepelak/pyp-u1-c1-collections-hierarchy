cbf57524bad5a0978777b2972b737f9850963bb6
Christine <christinecepelak@gmail.com>
2017-07-13 02:04:42 +0000

    proposed solution working

--- collections_hierarchy/node.py
+++ collections_hierarchy/node.py
@@ -1,3 +1,19 @@
 class Node(object):
     def __init__(self, value, next=None):
-        pass
+        self.value = value
+        self.next = next
+        
+s = Node('X', Node('Y', Node('Z')))
+
+print (s.value)
+
+#identifying the current node
+current = s
+
+#printing all nodes in the sequence
+while current is not None:
+    print(current.value)
+    current = current.next
+#using .next to iterate through the sequence
+print("--------")
+print(s.next.next.value)
